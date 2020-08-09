d3aaa2114215ee71b98c015425fd2b65900e0803
Your Name <samanbatool08@gmail.com>
2020-08-09 19:02:31 -0400

    material ui dependencies & input bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import '../css/Header.css';
+import { Link } from 'react-router-dom';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
     return (
         <nav className='header'>
             {/* amazon logo on the left (img tag) */}
-            <img 
-                className='header__logo'
-                src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
-                alt='amazon logo' 
-            />
+            <Link to='/'>
+                <img 
+                    className='header__logo'
+                    src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
+                    alt='amazon logo' 
+                />
+            </Link>
             {/* search box  */}
+            <input type='text' className='header__searchInput' />
+            <SearchIcon className='header__searchIcon' />            
             {/* 3 links - sign in/sign out, returns, prime account deets */}
             {/* basket - cart link */}
         </nav>
