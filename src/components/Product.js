4f1ddba4f6c2a2fa6c3651357db6571ca6d3b330
Your Name <samanbatool08@gmail.com>
2020-08-10 10:26:55 -0400

    building product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,22 +5,25 @@ import '../css/Product.css';
 function Product({ id, title, image, price, rating }) {
     return (
         <div className='product'>
-            <p>{title}</p>
-            <p className='product__price'>
-                <small>$</small>
-                <strong>{price}</strong>
-            </p>
-            <div className='product__rating'>
-                {
-                    Array(rating) 
-                    .fill()
-                    .map((_) => (
-                        <p></p>
-                    ))
-                }
-
+            <div className='product__info'>
+                <p>{title}</p>
+                <p className='product__price'>
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className='product__rating'>
+                    {
+                        Array(rating) 
+                        .fill()
+                        .map((_) => (
+                            <p>🍓</p>
+                        ))
+                    }
             </div>
-            
+        </div>
+            <img src={image} alt='{title}'/>
+            <button>Add to Basket</button>
+              
         </div>
     )
 }
