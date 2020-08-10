189ca46f246698a68755b1cea77f57ffdff59810
Your Name <samanbatool08@gmail.com>
2020-08-10 10:47:13 -0400

    dummy product data

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,13 +12,59 @@ function Home() {
             />
 
             {/* Product, id, title, price, rating, image */}
-            <Product 
-                id='12321341'
-                title='The Lean Startup: How Constant Innocation Creates Radically Successful Business Paperback'
-                price={11.96}
-                rating={5}
-                image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
-            />
+            <div className='home__row'>
+                <Product 
+                    id='12321341'
+                    title='The Lean Startup: How Constant Innocation Creates Radically Successful Business Paperback'
+                    price={11.96}
+                    rating={5}
+                    image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+                />
+
+                <Product 
+                    id='34651234'
+                    title='Electric Hand Mixer, MOSAIC 3 Speed Handhold Mixer with Turbo, Cord & Attachments Storage'
+                    price={21.99}
+                    rating={4}
+                    image="https://images-na.ssl-images-amazon.com/images/I/61d8dj%2BuV6L._AC_SL1500_.jpg"
+                />
+            </div>
+
+            <div className='home__row'>
+                <Product 
+                    id='86709833'
+                    title='Simple Modern 20oz Voyager Travel Mug Tumbler w/Clear Flip Lid & Straw'
+                    price={19.99}
+                    rating={4}
+                    image="https://images-na.ssl-images-amazon.com/images/I/71GBKF7qtQL._AC_SL1500_.jpg"
+                />
+
+                <Product 
+                    id='65437654'
+                    title='BLU Studio Mini -5.5HD Smartphone, 32GB+2GB Ram - Black'
+                    price={59.99}
+                    rating={4}
+                    image="https://images-na.ssl-images-amazon.com/images/I/A1gUduSRYZL._AC_SL1500_.jpg"
+                />
+
+                <Product 
+                    id='09780678'
+                    title='Willful Smart Watch for Android Phones and iOS Phones Compatible iPhone Samsung, IP68 Swimming Waterproof Smartwatch Fitness Tracker'
+                    price={35.99}
+                    rating={4}
+                    image="https://images-na.ssl-images-amazon.com/images/I/51oAPLRW9DL._AC_SL1000_.jpg"
+                />
+            </div>
+
+            <div className='home__row'>
+                <Product 
+                    id='72934749'
+                    title='Kindle Paperwhite – Now Waterproof with 2x the Storage – Ad-Supported'
+                    price={129.99}
+                    rating={5}
+                    image="https://images-na.ssl-images-amazon.com/images/I/61KWyBPz99L._AC_SL1000_.jpg"
+                />
+            </div>
         </div>
     )
 }
