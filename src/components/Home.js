3a67197363f28b6a98301d0b486ce65666ca4f9b
Your Name <samanbatool08@gmail.com>
2020-08-10 10:06:44 -0400

    set up product array

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../css/Home.css';
-
+import Product from './Product';
 
 function Home() {
     return (
@@ -9,6 +9,15 @@ function Home() {
             className='home__image'
             src='https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg' 
             alt='' 
+            />
+
+            {/* Product, id, title, price, rating, image */}
+            <Product 
+                id='12321341'
+                title='The Lean Startup: How Constant Innocation Creates Radically Successful Business Paperback'
+                price={11.96}
+                rating={5}
+                image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
             />
         </div>
     )
