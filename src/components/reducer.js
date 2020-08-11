54d78b7525d64295dd1205e4578ea3ab69ba48ed
Your Name <samanbatool08@gmail.com>
2020-08-11 12:10:25 -0400

    remove from basket working

--- src/components/reducer.js
+++ src/components/reducer.js
@@ -13,24 +13,21 @@ const reducer = (state, action) => {
     console.log(action);
     switch(action.type) {
         case 'ADD_TO_BASKET':
-            // Logic for adding item to basket
         return {
             ...state,
             basket: [...state.basket, action.item]
             };
         case 'REMOVE_FROM_BASKET':
-            // logic for removing items from basket
             let newBasket = [...state.basket]
             const index = state.basket.findIndex((basketItem) => basketItem.id === action.id)
-            
+            newBasket.splice(index, 1);
             if ( index >= 0) {
-
             }
             else {
                 console.warn(
-                    
-                )
-        return {...state};
+                    `Can't remove product (id: ${action.id}) as its not in the basket.`
+                )}
+        return {...state, basket: newBasket};
         default:
             return state;
     }
