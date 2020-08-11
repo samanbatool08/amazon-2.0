25b82529af735c2f37304160ff0e3ed45e563e91
Your Name <samanbatool08@gmail.com>
2020-08-10 23:18:07 -0400

    setup checkout page

---
+++ src/components/Checkout.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useStateValue } from './StateProvider';
+import '../css/Checkout.css';
+
+function Checkout() {
+
+    const [{ basket }] = useStateValue();
+    return (
+        <div className='checkout'>
+            <img 
+                className='checkout__ad'
+                src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg'
+                alt=''
+            />
+            {basket?.length === 0 (
+                <div>
+                    
+                    </div>
+            )}
+        </div>
+    )
+}
+
+export default Checkout;
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,7 @@ import { useStateValue } from './StateProvider';
 function Header() {
     const [{ basket }, dispatch ] = useStateValue();
 
+    console.log(basket)
 
     return (
         <nav className='header'>
