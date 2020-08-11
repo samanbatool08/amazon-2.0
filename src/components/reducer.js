5cb120a7d567bebb83413d87f6d6cd23dde8c02d
Your Name <samanbatool08@gmail.com>
2020-08-11 13:09:47 -0400

    subtotal calculating dynamically

--- src/components/reducer.js
+++ src/components/reducer.js
@@ -9,6 +9,11 @@ export const initialState = {
     user: null
 };
 
+
+export const getBasketTotal = (basket) => 
+    basket?.reduce((amount, item) => item.price + amount, 0);
+
+
 const reducer = (state, action) => {
     console.log(action);
     switch(action.type) {
