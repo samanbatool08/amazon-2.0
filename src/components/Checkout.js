e3d71ece53e0b0d876fd84a4e99e66a22a1567fc
Your Name <samanbatool08@gmail.com>
2020-09-27 15:38:24 -0400

    fixed typo

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -18,7 +18,7 @@ function Checkout() {
             />
             {basket?.length === 0 ? (
                 <div>
-                    <h2>Youur Shopping Basket is empty</h2>
+                    <h2>Your Shopping Basket is empty</h2>
                     <p>
                         You have no items in your basket. To buy one--or more--now, click the "Add to basket" next to the item.
                     </p>
